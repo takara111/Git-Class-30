5af8b0b4efddd11b5acef7c32d88dc471ae8acef
takara111 <taka160907@gmail.com>
2019-09-17 22:52:28 +0900

    first commit

---
+++ Test-4-2/Pods/Target Support Files/Pods-Test-4-2Tests/Pods-Test-4-2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_4_2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Test_4_2Tests
+@end
---
+++ Test-4-2/Test-4-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test-4-2
+//
+//  Created by 杉浦多可楽 on 2019/08/27.
+//  Copyright © 2019 多可楽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
